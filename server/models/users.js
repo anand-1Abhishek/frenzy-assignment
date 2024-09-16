07e52fdb87ccb6e8a782299b988ff5e10f43e024
Abhishek Anand <120253524+anand-1Abhishek@users.noreply.github.com>
2024-09-17 03:22:10 +0530

    Added User schema

---
+++ server/models/users.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  name: { type: String, required: true }, 
+  hobbies: [{ type: String }], 
+  bio: { type: String }, 
+  friends: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  friendRequests: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+});
+
+module.exports = mongoose.model('User', UserSchema);
